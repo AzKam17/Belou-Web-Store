0cf9825e5505ec0815c07f1bef5ab2e81840430e
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-04-24 07:16:13 +0000

    feat: fetching subdomain from url

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import {useEffect, useState} from "react";
 
 
 export default function Home() {
+  const [subdomain, setSubdomain] = useState<string>('')
+
+  useEffect(() => {
+    const host = window.location.hostname
+    const parts = host.split('.')
+    console.log(window.location.hostname)
+
+    setSubdomain(parts[0])
+  }, [])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,7 +29,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing{subdomain}{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
