6106a6b6720477b853952967142ca8c3cc4c49e7
AmadouAz <aziz.kamagate@djamo.io>
2025-10-01 21:31:12 +0000

    feat:convert to api

--- components/views/products/list-products.view.tsx
+++ components/views/products/list-products.view.tsx
@@ -30,11 +30,11 @@ export const ListProductsView = React.memo(function() {
 
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
-			{productsData?.map((e, idx) => (
+			{productsData.items.map((e, idx) => (
 				<div key={idx} className="flex flex-col items-center sm:items-start w-full group">
 					<Link href={`/product/${e.id}`} className="w-full">
 						<div className="overflow-hidden rounded-xl transition-transform duration-300 group-hover:scale-[1.02]">
-							<ListProductImageView imagePath={e.images[0]} />
+							<ListProductImageView imagePath={e.main_image} />
 						</div>
 						<div className="mt-3 space-y-1">
 							<h3 className="text-lg font-bold line-clamp-2 group-hover:text-primary transition-colors">{e.name}</h3>
